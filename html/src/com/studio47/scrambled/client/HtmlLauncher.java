7f55627105c5ff974e43517c75e5762ddb49bb42
Kyle Schick <kschick@umich.edu>
2016-07-17 23:43:16 -0700

    Updated .gitignore. Commited rest of initial files

---
+++ html/src/com/studio47/scrambled/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.studio47.scrambled.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.studio47.scrambled.Scrambled;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Scrambled();
+        }
+}
