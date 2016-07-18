9eba7c7445eff4baa10276083541d5c3075cf186
kschick7 <kschick@umich.edu>
2016-07-17 23:36:44 -0700

    Initial commit

---
+++ android/src/com/studio47/scrambled/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.studio47.scrambled;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.studio47.scrambled.Scrambled;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Scrambled(), config);
+	}
+}
---
+++ desktop/src/com/studio47/scrambled/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.studio47.scrambled.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.studio47.scrambled.Scrambled;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Scrambled(), config);
+	}
+}
